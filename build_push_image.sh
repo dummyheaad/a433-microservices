a16ea8c666e6c05e167401378a7340baa96df4cc
onbekend <uka412345@gmail.com>
2023-09-25 12:48:28 +0700

    add comment

--- build_push_image.sh
+++ build_push_image.sh
@@ -7,7 +7,7 @@ docker images
 # mengubah nama image agar sesuai dengan format GitHub Packages
 docker tag item-app:v1 ghcr.io/dummyheaad/item-app:v1
 
-# login ke GitHub Packages
+# login ke GitHub Packages dengan Personal Access Token
 echo $CR_PAT | docker login ghcr.io -u dummyheaad --password-stdin
 
 # unggah image ke GitHub Packages
